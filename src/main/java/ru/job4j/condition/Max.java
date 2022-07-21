d13b61f41696e51768e435a04af4ba3e29f3cbe5
nSurname <steel16253@yandex.ru>
2022-07-21 22:28:51 +0300

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,12 +1,13 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int first, int  second  ) {
-        int result = first >  second ? " left. ":" right. " ;
+    public static int max(int first, int  second) {
+        int result = first > second ? first : second;
         return result;
     }
+
     public static void main(String[] args) {
-        int msg = Max.max(3,2);
+        int msg = Max.max(3, 2);
         System.out.println(msg);
     }
 }
