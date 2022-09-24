f734ba352b218b2ebf7ff3b74a9c9034ee64d33e
nSurname <steel16253@yandex.ru>
2022-09-24 11:43:46 +0300

    4.3.5. Множественное логическое выражение И [#9461]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TriangleTest {
+
     @Test
      public void whenExist() {
         double ab = 2.0;
